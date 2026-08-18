b64bae2138ec2a1ca9439bf20d5223e1e70dc684
aaronstevenwhite <aaronstevenwhite@users.noreply.github.com>
2026-08-18 22:43:43 +0000

    deploy: panproto/panproto@efa04235eb9aa9d718ad358b6104f69eac17881f

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8259401e.js',
+                path_to_root + 'searchindex-e0e3e7c3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
